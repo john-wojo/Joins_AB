c0d7a2629b33a42df8d6c8fe8644f8592eec519a
John <john.w.wojtalewicz@gmail.com>
2026-07-13 19:48:29 -0800

    Added examples of inner joins to the file.

--- Inner_Joins_01.sql
+++ Inner_Joins_01.sql
@@ -1,2 +1,28 @@
 #Inner Joins
 
+select *
+from customers;
+
+select *
+from customers_orders;
+
+#Example of inner join 
+select *
+from customers as c
+inner join customer_orders as co
+	on c.customer_id = co.customer_id
+order by c.customer_id;
+
+#Example of inner join with aggregation function looking at how much each product has made the bakery
+select p.product_name, sum(order_total) as total 
+from products as p
+inner join customer_orders as co
+	on p.product_id = co.product_id
+group by p.product_name
+order by 2 desc;
+
+#Example of inner join when column names are not the same but the data is connected
+select *
+from suppliers s
+inner join ordered_items oi
+	on s.supplier_id = oi.shipper_id;

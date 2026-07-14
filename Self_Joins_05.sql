b6aa908fd648d0db1afeb5209ec01f36f5a60b37
John <john.w.wojtalewicz@gmail.com>
2026-07-13 20:59:49 -0800

    most recent save.

--- Self_Joins_05.sql
+++ Self_Joins_05.sql
@@ -1,2 +1,18 @@
 #Self Joins
 
+#Example of self join on unique column
+select *
+from customers c 
+join customers ss 
+	on c.first_name = ss.first_name;
+    
+#
+select c.customer_id,
+	   c.first_name,
+       c.last_name,
+       ss.customer_id,
+       ss.first_name,
+       ss.last_name
+from customers c 
+join customers ss
+	on c.customer_id + 1 = ss.customer_id;

c2fdb3dd445bf6ad5fdbc33069b0bebe5ff36e05
John <john.w.wojtalewicz@gmail.com>
2026-07-13 20:13:17 -0800

    Added example of joining three tables together in one query.

--- Joining_Multiple_Tables_02.sql
+++ Joining_Multiple_Tables_02.sql
@@ -1,2 +1,13 @@
 #Joining Multiple Tables
 
+#Example of joining more than one table together (chaining joins to connect tables that could not otherwise connect to)
+select p.product_name 'Product Name', 
+	   co.order_total 'Order Total', 
+       c.first_name 'First Name'
+from products p
+join customer_orders co
+	on p.product_id = co.product_id
+join customers c
+	on co.customer_id = c.customer_id
+where c.first_name = 'Kevin'
+order by co.order_total desc;
